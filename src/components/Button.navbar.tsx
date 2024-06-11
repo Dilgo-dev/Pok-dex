79c89832f820e973154952eaf3828c76507c3114
Benjamin <dilgopierre@gmail.com>
2024-06-11 14:24:31 +0200

    ✨ Add alert on pikachu !

--- src/components/Button.navbar.tsx
+++ src/components/Button.navbar.tsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 export default function ButtonNavBar({ index, setPokemonIndex, children }) {
     const handleClick = () => {
+        if (children.toLowerCase() === "pikachu") alert("Pika pikachu !!!");
         setPokemonIndex(index);
     }
 
