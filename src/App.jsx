5aa87f26a6f2e9e1ec8d64da272823eda0ae95b0
Benjamin <dilgopierre@gmail.com>
2024-06-11 11:33:05 +0200

    ✨ Add Previous and Next button and handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import './App.css'
+import { useState } from 'react';
 import PokemonCard from './components/PokemonCard'
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+import pokemonList from './utils/pokemonList';
+import './App.css'
 
 export default function App() {
+    const [pokemonIndex, setPokemonIndex] = useState(0);
+
+    const handlePrevious = () => {
+      setPokemonIndex(oldIndex => oldIndex - 1);
+    }
+    const handleNext = () => {
+      setPokemonIndex(oldIndex => oldIndex + 1);
+    }
     return (
         <div>
-            <PokemonCard pokemon={pokemonList[0]} />
+            <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+            <section>
+              { pokemonIndex > 0 ? <button onClick={handlePrevious}>Précédent</button> : null }
+              { pokemonIndex < pokemonList.length - 1 ? <button onClick={handleNext}>Suivant</button> : null }
+            </section>
         </div>
     );
 }
